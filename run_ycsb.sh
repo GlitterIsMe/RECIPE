aab2dd989bda2e57a2657ded396df82a74077f8a
zhangyiwen <zzyyyww9595@gmail.com>
2020-02-19 23:42:44 +0800

    add a script to run ycsb automatically

---
+++ run_ycsb.sh
@@ -0,0 +1,12 @@
+#!/bash/sh
+for INDEX in fastfair,masstree,art,hot,levelhash,clht; do
+    for WORKLOAD in a,b,c; do
+        echo "numactl --cpubind=1 ./build/ycsb-1kw $INDEX $WORKLOAD randint uniform 1"
+        #eval "numactl --cpubind=1 ./build/ycsb-1kw $INDEX $WORKLOAD randint uniform 1"
+    done
+done
+
+for INDEX in fastfair,masstree,art,hot; do
+        echo "numactl --cpubind=1 ./build/ycsb $INDEX e randint uniform 1 > $INDEX-e.log"
+        #eval "numactl --cpubind=1 ./build/ycsb $INDEX e randint uniform 1 > $INDEX-e.log"
+done
