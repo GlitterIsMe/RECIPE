5000dba7eda98a568b7bab38e6478075d58616e2
zhangyiwen <zzyyyww9595@gmail.com>
2020-02-19 23:50:47 +0800

    a small typo again

--- run_ycsb.sh
+++ run_ycsb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for INDEX in fastfair,masstree,art,hot,levelhash,clht; do
+for INDEX in fastfair masstree art hot levelhash clht; do
     for WORKLOAD in a,b,c; do
         echo "numactl --cpubind=1 ./build/ycsb-1kw $INDEX $WORKLOAD randint uniform 1"
         #eval "numactl --cpubind=1 ./build/ycsb-1kw $INDEX $WORKLOAD randint uniform 1"
     done
 done
 
-for INDEX in fastfair,masstree,art,hot; do
+for INDEX in fastfair masstree art hot; do
         echo "numactl --cpubind=1 ./build/ycsb $INDEX e randint uniform 1 > $INDEX-e.log"
         #eval "numactl --cpubind=1 ./build/ycsb $INDEX e randint uniform 1 > $INDEX-e.log"
 done
